3e010542a0de590a414376b4d171df21a40ae7e3
Andrew French <andrew.french@abletech.co.nz>
2017-06-16 12:01:39 +1200

    Fix typo; add CustomSettings object

--- src/aura/AddressFinder/AddressFinderController.js
+++ src/aura/AddressFinder/AddressFinderController.js
@@ -1,14 +1,14 @@
 ({
     afterScriptsLoaded : function(component, event, helper) {
        component.set("v.isScriptLoaded", true);
-       if(component.get("v.isRendered") == true) {
+       if(component.get("v.isRendered") === true) {
             helper.setupWidget(component);
         }
     },
     
     handleDoneRendering : function(component, event, helper) {
         component.set("v.isRendered", true);
-       if(component.get("v.isScriptLoaded") == true) {
+       if(component.get("v.isScriptLoaded") === true) {
             helper.setupWidget(component);
         }
     }
